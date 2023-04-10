8b9eead4207061ae3ec2286ab01e1de3b5181173
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-04-10 20:46:32 -0300

    es13

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,10 @@
+const string =
+  'JavaScript es un maravilloso lenguaje de programacion y JavaScript la rompe';
+
+const replacedString = string.replace('JavaScript', 'TypeScript');
+
+console.log(replacedString);
+
+const replacedString2 = string.replaceAll('JavaScript', 'TypeScript');
+
+console.log(replacedString2);
